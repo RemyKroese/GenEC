00d523e55e6a44a254382afb68ae66a456572d7a
RemyKroese <rkroese96@gmail.com>
2025-01-10 09:25:36 +0100

    Fix template bugs (#9)

--- tests/unit_tests/test_extractor.py
+++ tests/unit_tests/test_extractor.py
@@ -36,30 +36,30 @@ def extractor_instance():
     (BASIC_TEXT, '\n', ['a b c', 'd e', 'f g h i', 'j']),
     (WHITELINES_TEXT, '\n\n', ['a b c\nd e', 'f g h i', 'j\nk l m n o p\nq'])])
 def test_get_clusters(extractor_instance, data, cluster_filter, expected_result):
-    extractor_instance.config[ConfigOptions.CLUSTER_FILTER] = cluster_filter
-    assert extractor_instance.get_clusters(data, Files.SOURCE) == expected_result
+    extractor_instance.config[ConfigOptions.CLUSTER_FILTER.value] = cluster_filter
+    assert extractor_instance.get_clusters(data, Files.SOURCE.value) == expected_result
 
 
 def test_extract_text_from_clusters_by_regex(extractor_instance):
     clusters = ['saiucdjh1', 'dusi2hiuw', '3134ferw', '4waijc', 'djhe56fk7', 'iuaijaudc']
-    extractor_instance.config[ConfigOptions.TEXT_FILTER] = r'^[^\d]*(\d)'
+    extractor_instance.config[ConfigOptions.TEXT_FILTER.value] = r'^[^\d]*(\d)'
     assert extractor_instance.extract_text_from_clusters_by_regex(clusters) == ['1', '2', '3', '4', '5']
 
 
 def test_extract_from_data(extractor_instance):
     data = 'saiucdjh1\ndusi2hiuw\n3134ferw\n4waijc\ndjhe56fk7\niuaijaudc'
-    extractor_instance.config[ConfigOptions.TEXT_FILTER_TYPE] = TextFilterTypes.REGEX.value
-    extractor_instance.config[ConfigOptions.TEXT_FILTER] = r'^[^\d]*(\d)'
-    assert extractor_instance.extract_from_data(data, Files.SOURCE) == ['1', '2', '3', '4', '5']
+    extractor_instance.config[ConfigOptions.TEXT_FILTER_TYPE.value] = TextFilterTypes.REGEX.value
+    extractor_instance.config[ConfigOptions.TEXT_FILTER.value] = r'^[^\d]*(\d)'
+    assert extractor_instance.extract_from_data(data, Files.SOURCE.value) == ['1', '2', '3', '4', '5']
 
 
 def test_extract_from_data_unsupported_filter_type(extractor_instance):
     data = 'saiucdjh1\ndusi2hiuw\n3134ferw\n4waijc\ndjhe56fk7\niuaijaudc'
-    extractor_instance.config[ConfigOptions.CLUSTER_FILTER] = '\n'
-    extractor_instance.config[ConfigOptions.TEXT_FILTER_TYPE] = TextFilterTypes.KEYWORD.value
-    extractor_instance.config[ConfigOptions.TEXT_FILTER] = r'^[^\d]*(\d)'
+    extractor_instance.config[ConfigOptions.CLUSTER_FILTER.value] = '\n'
+    extractor_instance.config[ConfigOptions.TEXT_FILTER_TYPE.value] = TextFilterTypes.KEYWORD.value
+    extractor_instance.config[ConfigOptions.TEXT_FILTER.value] = r'^[^\d]*(\d)'
     with pytest.raises(ValueError, match='Unsupported filter type: %s' % TextFilterTypes.KEYWORD.value):
-        extractor_instance.extract_from_data(data, Files.SOURCE)
+        extractor_instance.extract_from_data(data, Files.SOURCE.value)
 
 
 @pytest.mark.parametrize('input_side_effects, clusters,expected_result', [
@@ -85,18 +85,18 @@ def test_get_sliced_clusters(extractor_instance, input_side_effects, clusters, e
 def test_get_src_clusters_with_slicing(extractor_instance):
     data = 'This is the first cluster\nThis is the start cluster\nThis is another cluster\nThis is the end cluster\nThis is the last cluster'
     expected_result = ['This is the start cluster', 'This is another cluster', 'This is the end cluster']
-    extractor_instance.config[ConfigOptions.CLUSTER_FILTER] = '\n'
-    extractor_instance.config[ConfigOptions.SHOULD_SLICE_CLUSTERS] = True
-    extractor_instance.config[ConfigOptions.SRC_START_CLUSTER_TEXT] = 'start'
-    extractor_instance.config[ConfigOptions.SRC_END_CLUSTER_TEXT] = 'end'
-    assert extractor_instance.get_clusters(data, Files.SOURCE) == expected_result
+    extractor_instance.config[ConfigOptions.CLUSTER_FILTER.value] = '\n'
+    extractor_instance.config[ConfigOptions.SHOULD_SLICE_CLUSTERS.value] = True
+    extractor_instance.config[ConfigOptions.SRC_START_CLUSTER_TEXT.value] = 'start'
+    extractor_instance.config[ConfigOptions.SRC_END_CLUSTER_TEXT.value] = 'end'
+    assert extractor_instance.get_clusters(data, Files.SOURCE.value) == expected_result
 
 
 def test_get_ref_clusters_with_slicing(extractor_instance):
     data = 'This is the first cluster\nThis is the start cluster\nThis is another cluster\nThis is the end cluster\nThis is the last cluster'
     expected_result = ['This is the start cluster', 'This is another cluster', 'This is the end cluster']
-    extractor_instance.config[ConfigOptions.CLUSTER_FILTER] = '\n'
-    extractor_instance.config[ConfigOptions.SHOULD_SLICE_CLUSTERS] = True
-    extractor_instance.config[ConfigOptions.REF_START_CLUSTER_TEXT] = 'start'
-    extractor_instance.config[ConfigOptions.REF_END_CLUSTER_TEXT] = 'end'
-    assert extractor_instance.get_clusters(data, Files.REFERENCE) == expected_result
+    extractor_instance.config[ConfigOptions.CLUSTER_FILTER.value] = '\n'
+    extractor_instance.config[ConfigOptions.SHOULD_SLICE_CLUSTERS.value] = True
+    extractor_instance.config[ConfigOptions.REF_START_CLUSTER_TEXT.value] = 'start'
+    extractor_instance.config[ConfigOptions.REF_END_CLUSTER_TEXT.value] = 'end'
+    assert extractor_instance.get_clusters(data, Files.REFERENCE.value) == expected_result
