bb2784c358ae81fc307fb46180f5580d6ca02f6c
RemyKroese <rkroese96@gmail.com>
2025-07-15 15:07:10 +0200

    configurable presets directory parameter (#18)

--- GenEC/main.py
+++ GenEC/main.py
@@ -3,7 +3,8 @@ import argparse
 import os
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(PROJECT_PATH)
 
 from GenEC import utils                     # noqa: E402
 from GenEC.core import analyze, manage_io   # noqa: E402
@@ -12,15 +13,19 @@ from GenEC.core import FileID               # noqa: E402
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
-        description='Extract specific data from files and' +
-        'and compare the differences between these 2 files based on the data.')
+        description='Extract specific data from files and ' +
+        'compare the differences between these two files based on the data.')
 
     parser.add_argument('-s', '--source', type=str, required=True,
                         help='Source file to extract data from.')
     parser.add_argument('-r', '--reference', type=str, required=False,
                         help='Reference file to extract data from and compare against source.')
-    parser.add_argument('-o', '--output_directory', type=str, required=False,
-                        help='Output directory to store results.')
+    parser.add_argument('-o', '--output-directory', type=str, required=False,
+                        help='Output directory to store results (json and txt).')
+
+    parser.add_argument('-x', '--presets-directory', type=str, required=False,
+                        default=os.path.abspath(os.path.join(PROJECT_PATH, 'GenEC', 'presets')),
+                        help='Directory where presets are stored. default: %(default)s')
 
     # Only a preset, or a preset-list should be provided
     preset_group = parser.add_mutually_exclusive_group(required=False)
@@ -41,7 +46,7 @@ def main():
     elif args.preset:
         preset_param = {'type': 'preset', 'value': args.preset}
 
-    input_manager = manage_io.InputManager(preset_param)
+    input_manager = manage_io.InputManager(preset_param, args.presets_directory)
     input_manager.set_config()
     extractor = analyze.Extractor(input_manager.config)
     output_manager = manage_io.OutputManager(args.output_directory)
